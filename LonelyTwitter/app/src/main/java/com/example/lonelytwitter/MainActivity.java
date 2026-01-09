a48e884178accab8747738c3d5c6372f35bcd761
treya678 <puthumba@ualberta.ca>
2026-01-08 18:13:13 -0700

    lab1

--- LonelyTwitter/app/src/main/java/com/example/lonelytwitter/MainActivity.java
+++ LonelyTwitter/app/src/main/java/com/example/lonelytwitter/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
